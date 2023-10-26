a6d8708902c78b6f7d0bfb70578660c0fa745179
Will Tsai <wntsai@ucsc.edu>
2023-10-25 17:27:55 -0700

    Copied into New Repo

---
+++ src/main.js
@@ -0,0 +1,15 @@
+let config = {
+    type: Phaser.AUTO,
+    width: 640,
+    height: 480,
+    scene: [Menu, Play ]
+}
+
+let game = new Phaser.Game(config);
+
+//reserve keyboard vars
+let keyF, keyR, keyLEFT, keyRIGHT;
+
+//UI sizes set-up
+let borderUISize = game.config.height / 15;
+let borderPadding = borderUISize / 3;
